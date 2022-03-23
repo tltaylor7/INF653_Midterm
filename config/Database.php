524b15c9a54463ffa1bebfc6d23f32034e7fce59
tltaylor7 <89573255+tltaylor7@users.noreply.github.com>
2022-03-23 15:45:46 -0400

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -4,21 +4,23 @@
         private $conn;
 
         function __construct() {
-            $this->$url = getenv('JAWSDB_URL');
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
             $this->conn = null;
+            
         }
         
         //DB Connect
         public function connect() {
             
-            $dbparts = parse_url($this->$url);
             
-            $hostname = $dbparts['host'];
-            $username = $dbparts['user'];
-            $password = $dbparts['pass'];
-            $database = ltrim($dbparts['path'],'/');
+            try {
+                    $hostname = $dbparts['host'];
+                    $username = $dbparts['user'];
+                    $password = $dbparts['pass'];
+                    $database = ltrim($dbparts['path'],'/');
     
-                try {
+                
                     $this->$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
                     // set the PDO error mode to exception
                     $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
