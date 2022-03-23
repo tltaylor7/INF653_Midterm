6add8d0bd63dfe7d752ef8aac34eea26c334f03a
Travis Taylor <tltaylor7@mail.fhsu.edu>
2022-03-23 15:33:19 -0400

    hope it fixes

--- config/Database.php
+++ config/Database.php
@@ -1,34 +1,33 @@
 <?php 
-    class Database {
-        //DB param
-        
-        
+    class Database {  
+        private $url;
+        private $conn;
+
         function __construct() {
-            $url = getenv('JAWSDB_URL');
+            $this->$url = getenv('JAWSDB_URL');
             $this->conn = null;
         }
         
         //DB Connect
         public function connect() {
             
-            $dbparts = parse_url($url);
-
+            $dbparts = parse_url($this->$url);
+            
             $hostname = $dbparts['host'];
             $username = $dbparts['user'];
             $password = $dbparts['pass'];
             $database = ltrim($dbparts['path'],'/');
-
-            
-            try {
-                $this->$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-                // set the PDO error mode to exception
-                $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected successfully";
-                }
-            catch(PDOException $e)
-                {
-                echo "Connection failed: " . $e->getMessage();
-                }
+    
+                try {
+                    $this->$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                    // set the PDO error mode to exception
+                    $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    echo "Connected successfully";
+                    }
+                catch(PDOException $e)
+                    {
+                    echo "Connection failed: " . $e->getMessage();
+                    }
             return $this->conn;
         }
     }
